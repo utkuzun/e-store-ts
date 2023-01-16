7ff05b12518502967651e963b6acfb9ee543e43a
utkuzun <utkuzun50@gmail.com>
2023-01-17 02:18:13 +0300

    password hash middleware added

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -1,4 +1,19 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  if (params.action == 'create' && params.model == 'User') {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const user = params.args.data;
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const hashPassword = await bcrypt.hash(user.password, salt);
+    params.args.data.password = hashPassword;
+  }
+  return next(params);
+});
 
 export default prisma;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,16 @@
-import prisma from '../db/prismaClient';
-import { PrismaClient } from '@prisma/client';
-import userSchema, { UserValidation } from '../schemas/userSchema';
+// import prisma from '../db/prismaClient';
+// import { PrismaClient } from '@prisma/client';
+// import userSchema, { UserValidation } from '../schemas/userSchema';
 
-const Users = (prismaUser: PrismaClient['user']) => {
-  return Object.assign(prismaUser, {
-    validate(userData: unknown): UserValidation {
-      const result = userSchema.parse(userData);
-      return result;
-    },
-  });
-};
+// const Users = (prismaUser: PrismaClient['user']) => {
+//   return Object.assign(prismaUser, {
+//     validate(userData: unknown): UserValidation {
+//       const result = userSchema.parse(userData);
+//       return result;
+//     },
+//   });
+// };
 
-const User = Users(prisma.user);
+// const User = Users(prisma.user);
 
-export default User;
+// export default User;
