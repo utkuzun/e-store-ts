ef657db6e60a50e8d131ff80e06d888fe95cb1a3
utkuzun <utkuzun50@gmail.com>
2023-01-30 15:24:51 +0300

    morgan update

--- src/middleware/morgan.ts
+++ src/middleware/morgan.ts
@@ -1,9 +1,13 @@
+import { Response, Request } from 'express';
 import morgan from 'morgan';
 
-// morgan.token('body', (req, res) => {
-//   JSON.stringify(req.body);
-// });
+morgan.token('body', (req: Request, _res: Response) => {
+  if (req.body.password) {
+    delete req.body.password;
+  }
+  return JSON.stringify(req.body);
+});
 
 export default morgan(
-  ':method :url :status :res[content-length] - :response-time ms'
+  ':method :url :status :res[content-length] - :response-time ms :body'
 );
